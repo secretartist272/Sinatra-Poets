15155e230f4816695ee9ceb682bdd13dba881622
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-14 01:15:51 -0500

    added session_controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,23 @@
+class SessionController < ApplicationController
+    #login
+    get '/login' do
+        erb :"sessions/new"
+    end
+
+    post '/login' do 
+        user = user.find_by(username: params[:user][:username])
+
+        if user && user.authenticate(params[:user][:username])
+            session[:user_id] = user.user_id
+            redirect to "/users/#{user.id}"
+        else
+            
+        end
+    end
+
+    #logout
+    post '/logout' do
+        session.clear
+        redirect to '/'
+    end
+end
