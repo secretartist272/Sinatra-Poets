5db4f029067753bd8a7648eadc94b0f50689c5d9
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-24 16:19:02 +0000

    poem controller update

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -5,5 +5,39 @@ class PoemsController < ApplicationController
 
         erb :"/poems/index"
     end
+    
+    get '/poems/new' do
+      erb :"/poems/new"
+    end
+    
+    post '/poems' do
+      @poem = Poem.create(:title => params[:title], :content => params[:content])
+      redirect to "/poems/#{@poem.id}"
+    end
+    
+    get '/poems/:id' do
+      @poem = Poem.find_by_id(params[:id])
+      erb :"/poems/show"
+    end
+    
+    get '/poems/:id/edit' do
+      @poem = Poem.find_by_id(params[:id])
+      @poem.title = params[:title]
+      @poem.content = params[:content]
+      @poem.save
+      redirect to "/poems/#{@poem.id}"
+    end
+    
+    patch '/poems/:id' do
+      
+    end
+    
+    put '/poems/:id' do
+      
+    end
+    
+    delete '/poems/:id' do 
+      
+    end
 
 end
