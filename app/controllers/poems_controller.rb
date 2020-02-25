ac430153847be9343274cd5c93f911c24e46392c
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-24 22:31:11 -0500

    update poems controller

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,54 +1,79 @@
 class PoemsController < ApplicationController
 
-    #index action 
-    get '/poems' do
-        @poems = Poem.all 
+  #index action 
+  get '/poems' do
+      @poems = Poem.all 
 
-        erb :"/poems/index"
-    end
-    
-    # new action 
-    get '/poems/new' do
-      erb :"/poems/new"
-    end
-    
-    # create action 
-    post '/poems' do
+      erb :"/poems/index"
+  end
+  
+  # new action 
+  get '/poems/new' do
+    redirect_if_not_logged_in
+    erb :"/poems/new"
+  end
+  
+  # create action 
+  post '/poems' do
+    redirect_if_not_logged_in
+
+    if params[:title] && params[:content] != ""
       @poem = Poem.create(:title => params[:title], :content => params[:content])
       redirect to "/poems/#{@poem.id}"
+    else  
+      redirect '/poems/new'
     end
+  end
+  
+  # show action
+  get '/poems/:id' do
+    set_poem
+    erb :'/poems/show'
+  end
+  
+  # edit action
+  get '/poems/:id/edit' do
+    redirect_if_not_logged_in
+    set_poem
     
-    # show action
-    get '/poems/:id' do
-      @poem = Poem.find_by_id(params[:id])
-      erb :"/poems/show"
-    end
-    
-    # edit action
-    get '/poems/:id/edit' do
-      @poem = Poem.find_by_id(params[:id])
+    if authorized_to_edit?(@poem)
       erb :"/poems/edit"
+    else  
+      redirect "users/#{current_user.id}"
     end
+  end
     
-    # update action 
-    patch '/poems/:id' do
-      @poem = Poem.find_by_id(params[:id])
-      @poem.title = params[:title]
-      @poem.content = params[:content]
-      @poem.save
-      redirect to "/poems/#{@poem.id}"
-    end
-    
-    # update action
-    # put '/poems/:id' do
-      
-    # end
-    
-    # delete action
-    delete '/poems/:id' do 
-      @poem = Poem.find_by_id(params[:id])
-      @poem.delete
-      redirect to '/poems'
-    end
+  # update action 
+  patch '/poems/:id' do
+    redirect_if_not_logged_in
+    set_poem
 
+    if @poem.user == current_user && params[:content] != ""
+      @poem.update(content: params[:content]))
+      redirect to "/poems/#{@poem.id}"
+    else 
+      redirect "users/#{current_user.id}"
+    end
+  end
+    
+   
+    
+  # delete action
+  delete '/poems/:id' do 
+    set_poem
+    
+    if authorized_to_edit?(@poem)
+      @poem.destroy
+      redirect '/poems'
+    else
+      redirect '/poems'
+    end
+  end
+
+  helper do
+
+    def set_poem
+      @poem = Poem.find(params[:id])
+    end
+  end
 end
